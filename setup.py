c3f01b04483ea621785e6a30d29474461b4e5c78
Nicolas Vetsch <vetschn@babba.ee.ethz.ch>
2022-04-07 10:29:17 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name="ntcad",
+    version="0.0.1a1",
+    author="Nicolas Vetsch",
+    author_email="vetschn@iis.ee.ethz.ch",
+    description="",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/vetschn/eclabfiles",
+    project_urls={"Bug Tracker": "https://github.com/vetschn/eclabfiles/issues"},
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords=["Nano-TCAD", "VASP", "OMEN", "Wannier90"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[],
+    python_requires=">=3.9",
+)
