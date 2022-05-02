d8007392e309518721714d3f384ce14153f187f0
vetschn <vetschn@iis.ee.ethz.ch>
2022-05-02 15:42:45 +0200

    Winterface interface winput writer

--- src/ntcad/calculators/winterface.py
+++ src/ntcad/calculators/winterface.py
@@ -5,21 +5,63 @@ an interface to run Winterface jobs.
 """
 
 import os
+import subprocess
 
 from ntcad.calculators.calculator import Calculator
+from ntcad.io.winterface import write_winput
 
 
 class Winterface(Calculator):
-    def __init__(self, directory: os.PathLike, **kwargs: dict) -> None:
-        super().__init__(directory, **kwargs)
+    """_summary_
+
+    Parameters
+    ----------
+    Calculator
+        _description_
+    """
+
+    def __init__(self, directory: os.PathLike, **winput_tags: dict) -> None:
+        """_summary_
+
+        Parameters
+        ----------
+        directory
+            _description_
+        """
+        self.directory = directory
+        self.winput_tags = winput_tags
 
     def calculate(self, command: str) -> None:
-        return super().calculate(command)
+        """_summary_
 
-    def _write_input(self) -> None:
-        return super()._write_input()
+        Parameters
+        ----------
+        command
+            _description_
 
-    def _read_output(self) -> None:
-        return super()._read_output()
+        Returns
+        -------
+            _description_
+        """
+        # Check if input is written.
+        if not os.path.exists(os.path.join(self.directory, "winput")):
+            self.write_input()
+
+        # TODO: Don't really like the shell=True here.
+        retcode = subprocess.call(command, shell=True, cwd=self.directory)
+        return retcode
+
+    def write_input(self) -> None:
+        """_summary_"""
+        write_winput(self.directory, **self.winput_tags)
+
+    def read_output(self) -> None:
+        """_summary_
+
+        Returns
+        -------
+            _description_
+        """
+        ...
 
     pass
--- src/ntcad/io/winterface.py
+++ src/ntcad/io/winterface.py
@@ -3,6 +3,7 @@
 """
 
 import os
+import numpy as np
 
 
 def write_winput(path: os.PathLike, **winput_tags: dict) -> None:
@@ -13,4 +14,29 @@ def write_winput(path: os.PathLike, **winput_tags: dict) -> None:
     path
         _description_
     """
-    pass
+    lines = ["# winput written by ntcad\n"]
+
+    for tag, value in winput_tags.items():
+        line = tag
+        if isinstance(value, (list, tuple, np.ndarray)):
+            array = np.array(value)
+            if array.ndim == 1:
+                line += " = " + " ".join(list(map(str, value)))
+            elif array.ndim == 2:
+                # Transformation matrices and k-points are a little
+                # special.
+                line += "\n"
+                for i in range(array.shape[0]):
+                    for j in range(array.shape[1]):
+                        line += "{:10.5f}".format(array[i, j])
+                    line += "\n"
+            else:
+                raise ValueError(
+                    f"Value can only be a 1D or 2D array. Instead got: {value=}"
+                )
+        else:
+            line += " = " + str(value)
+        lines.append(line + "\n")
+
+    with open(os.path.join(os.path.dirname(path), "winput"), "w") as winput:
+        winput.writelines(lines)
