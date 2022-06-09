9130e68e950b46acaba0d23ceab56b5b6b984953
vetschn <vetschn@iis.ee.ethz.ch>
2022-06-09 22:32:46 +0200

    Cleanup

--- src/ntcad/omen/__init__.py
+++ src/ntcad/omen/__init__.py
@@ -1,7 +1,8 @@
 from ntcad.omen import io
 from ntcad.omen.calculator import OMEN
 from ntcad.omen.operations import (
-    max_nearest_neighbors,
+    max_nn,
     photon_scattering_matrix,
+    photon_scattering_matrix_large,
     split_H_matrices,
 )
--- src/ntcad/vasp/__init__.py
+++ src/ntcad/vasp/__init__.py
@@ -1,2 +1,3 @@
 from ntcad.vasp import io
 from ntcad.vasp.calculator import VASP
+from ntcad.vasp.visualization import plot_bands
